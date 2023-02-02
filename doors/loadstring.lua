13503052856b48b4dbeb9f47447bc61f31a624e2
OminousVibes-Exploit <87617218+OminousVibes-Exploit@users.noreply.github.com>
2023-02-02 17:55:13 -0500

    Update loadstring.lua

--- doors/loadstring.lua
+++ doors/loadstring.lua
@@ -1,36 +1,8 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
--- Constants:
-local LOADSTRING_URL = "https://raw.githubusercontent.com/OminousVibes-Exploit/Scripts/main/doors/loadstring.lua"
-local GAME_ID = game.GameId
-local PLACE_ID = game.PlaceId
-
--- Execution Checks:
-if GAME_ID ~= 2440500124 then
-	return
-end
-if not game:IsLoaded() then
-	game.Loaded:Wait()
-end
-task.wait(2.5) -- Buffer Wait
-
--- Script Switch:
-local floor = ReplicatedStorage:WaitForChild("GameData", 10):WaitForChild("Floor", 10).Value
-if PLACE_ID == 6516141723 then -- Lobby
-	-- Lobby Script
-elseif PLACE_ID == 6839171747 then -- Game
-	if floor == "Hotel" then
-		loadstring(
-			game:HttpGet("https://raw.githubusercontent.com/OminousVibes-Exploit/Scripts/main/doors/places/hotel.lua")
-		)()
-	elseif floor == "Rooms" then
-		loadstring(
-			game:HttpGet("https://raw.githubusercontent.com/OminousVibes-Exploit/Scripts/main/doors/places/rooms.lua")
-		)()
-	else
-		print(floor)
-	end
-end
-
--- Auto Execute
-syn.queue_on_teleport('loadstring(game:HttpGet("' .. LOADSTRING_URL .. '"))()')
+local a=game:GetService'ReplicatedStorage'local b=
+[[https://raw.githubusercontent.com/OminousVibes-Exploit/Scripts/main/doors/]]
+local c=game.GameId local d=game.PlaceId if c~=2440500124 then return end if not
+game:IsLoaded()then game.Loaded:Wait()end task.wait(2.5)local e=a:WaitForChild(
+'GameData',10):WaitForChild('Floor',10).Value if d==6516141723 then elseif d==
+6839171747 then if e=='Hotel'then loadstring(game:HttpGet(b..'places/hotel.lua')
+)()elseif e=='Rooms'then loadstring(game:HttpGet(b..'places/rooms.lua'))()end
+end syn.queue_on_teleport('loadstring(game:HttpGet("'..b..'loadstring.lua"))()')
