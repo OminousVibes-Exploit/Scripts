da79e5909eaaf75b8cd9b9773f6a13770ea55d8d
OminousVibes-Exploit <87617218+OminousVibes-Exploit@users.noreply.github.com>
2023-02-03 23:47:08 -0500

    Update loadstring.lua

--- doors/loadstring.lua
+++ doors/loadstring.lua
@@ -1,8 +1,9 @@
 local a=game:GetService'ReplicatedStorage'local b=
 [[https://raw.githubusercontent.com/OminousVibes-Exploit/Scripts/main/doors/]]
 local c=game.GameId local d=game.PlaceId if c~=2440500124 then return end if not
-game:IsLoaded()then game.Loaded:Wait()end task.wait(5)local e=a:WaitForChild(
+game:IsLoaded()then game.Loaded:Wait()task.wait(5)end local e=a:WaitForChild(
 'GameData',10):WaitForChild('Floor',10).Value if d==6516141723 then elseif d==
 6839171747 then if e=='Hotel'then loadstring(game:HttpGet(b..'places/hotel.lua')
 )()elseif e=='Rooms'then loadstring(game:HttpGet(b..'places/rooms.lua'))()end
-end syn.queue_on_teleport('loadstring(game:HttpGet("'..b..'loadstring.lua"))()')
+end local f=(syn and syn.queue_on_teleport)or syn.queue_on_teleport if f then f(
+'loadstring(game:HttpGet("'..b..'loadstring.lua"))()')end
